267d66cbdae99531e87dc3c0cd07fec7274c9c31
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 10:24:23 +0600

    add

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ let a = 12;
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
 // let arr: number[] = [1, 2, 3, 4];
 // let arr: [string, number] = ["umayer",21]
-let arr = [21, "umayer"];
+// let arr: [number, string] = [21,"umayer"]
+var userRules;
+(function (userRules) {
+    userRules["ADMIN"] = "admin";
+    userRules["GUEST"] = "guest";
+    userRules["SUPER_ADMIN"] = "super_admin";
+})(userRules || (userRules = {}));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -2,4 +2,10 @@ let a = 12
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
 // let arr: number[] = [1, 2, 3, 4];
 // let arr: [string, number] = ["umayer",21]
-let arr: [number, string] = [21,"umayer"]
+// let arr: [number, string] = [21,"umayer"]
+
+enum userRules {
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super_admin"
+}
