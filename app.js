8f7ee94fd56ab1550dd37f7a58328cb95193b5fe
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 08:54:08 +0600

    add

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var a = 10;
-var b = 20;
+const b = 20;
 console.log(a, b);
 console.log(a, b);
 //# sourceMappingURL=app.js.map
