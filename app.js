ce1ae0d37b358e994d29318bc0b5e2cd4d457bf4
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 12:18:13 +0600

    add

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let a = 12;
+// let a = 12
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
 // let arr: number[] = [1, 2, 3, 4];
 // let arr: [string, number] = ["umayer",21]
 // let arr: [number, string] = [21,"umayer"]
-var userRules;
-(function (userRules) {
-    userRules["ADMIN"] = "admin";
-    userRules["GUEST"] = "guest";
-    userRules["SUPER_ADMIN"] = "super_admin";
-})(userRules || (userRules = {}));
+Object.defineProperty(exports, "__esModule", { value: true });
+// enum userRules {
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "super_admin"
+// }
+let a;
+a = 10;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
-let a = 12
+// let a = 12
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
 // let arr: number[] = [1, 2, 3, 4];
 // let arr: [string, number] = ["umayer",21]
 // let arr: [number, string] = [21,"umayer"]
 
-enum userRules {
-    ADMIN = "admin",
-    GUEST = "guest",
-    SUPER_ADMIN = "super_admin"
-}
+// enum userRules {
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "super_admin"
+// }
+
+let a:number;
+a = 10
