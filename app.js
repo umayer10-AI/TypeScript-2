6e5154285ecf4fe5c626256c492acc265c827e9b
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 09:58:49 +0600

    add

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 12;
+let arr = [1, 2, 3, 'Umayer'];
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,3 @@
 let a = 12
+
+let arr = [1,2,3,'Umayer']
