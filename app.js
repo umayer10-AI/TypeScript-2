0c8be09c9942c8c3206fa0ec09395ec1d7bc5abb
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 10:07:47 +0600

    add

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let a = 12;
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
-let arr = [1, 2, 3, 4];
+// let arr: number[] = [1, 2, 3, 4];
+let arr = ["umayer", 21];
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
 let a = 12
 // let arr = [1,2,3,{name: 'Umayer'}, {name: 22}
-let arr: number[] = [1, 2, 3, 4];
+// let arr: number[] = [1, 2, 3, 4];
+let arr: [string, number] = ["umayer",21]
